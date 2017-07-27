2138d3e3ad61244a5d513a09ab2b881cc0157a90
Edgar Guzman <eguzman@basecampcodingacademy.org>
2017-07-27 13:33:54 -0500

    I changed the varaible name and made them with no spaces and renamed every single one of them.

--- core.py
+++ core.py
@@ -4,48 +4,48 @@ def count_price(items, amount):
     Adds up the price of the amount of items of the purachase.
     Reurns the total price.
     """
-    JBL DJ speakers = 80
-    JBL stage speakers = 120
-    JBL bass stage speakers = 135
-    Numark NVII Dual Display USB DJ Controller = 600
-    Numark Mixtrack Platinum DJ Controller = 185
+    djspeakers = 80
+    stagespeakers = 120
+    subspeakers = 135
+    djController = 600
+    mixingboard = 185
 
     if  items == '1':
-        return float(JBL DJ speakers * amount)
+        return float(djspeakers * amount)
     elif  items == '2':
-        return float(JBL stage speakers * amount) 
+        return float(stagespeakers * amount) 
     elif  items == '3':
-        return float(JBL bass stage speakers * amount) 
+        return float(subspeakers * amount) 
     elif  items == '4':
-        return float(Numark NVII Dual Display USB DJ Controller * amount) 
+        return float(djController * amount) 
     elif  items == '5':
-        return float(Numark Mixtrack Platinum DJ Controller * amount) 
+        return float(mixingboard * amount) 
 
 def make_inventory():
     """ () -> List[(str, int)]
     Take the file an return the file into a list
 
     >>> make_inventory()
-    [['JBL DJ speakers, 56], [JBL stage speakers, 60], [JBL bass stage speakers, 60], 
-    [ Numark NVII Dual Display USB DJ Controller, 20], [Numark Mixtrack Platinum DJ Controller, 23]]
+    [[djspeakers = 56], [stagespeakers = 60], [subspeakers = 60], 
+    [ djController = 20], [mixingboard = 23]]
     """
     Equipment = []
     with open('inventory.txt', 'r') as file:
         for each in Equipment:
-        split_string = line.split(', ')
-        Equipment.append([split_string[0], int(split_string[1])])
-    return Equipment
+            split_string = line.split(', ')
+            Equipment.append([split_string[0], int(split_string[1])])
+        return Equipment
 
 def pretty_inventory(Equipment):
     """ list -> str
     Take the list and retrns it back to a string
 
     >>> pretty_incentory(Equipment)
-    1. JBL DJ speakers, 56
-    2. JBL stage speakers, 60
-    3. JBL bass stage speakers, 60
-    4. Numark NVII Dual Display USB DJ Controller, 20
-    5. Numark Mixtrack Platinum DJ Controller, 23
+    1. djspeakers, 56
+    2. stagespeakers, 60
+    3. subspeakers, 60
+    4. djController, 20
+    5. mixingboard, 23
 
     """
     message = ''
