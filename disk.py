4eb7bd043df7e1f3b8f05fd413cc59b47a1bdc0f
Edgar Guzman <eguzman@basecampcodingacademy.org>
2017-07-27 13:37:31 -0500

    I made two functions one named in_the_history that opens the histoty and shows it and the other named in_the_inventory that tells what is in the inventory.

---
+++ disk.py
@@ -0,0 +1,9 @@
+def in_the_histoy():
+    left = []
+    with open('log.txt', 'r') as file:
+        file.readline()
+        lines = file.readlines()
+    for line in lines:
+        split_string = line.strip().split(', ')
+        left.append([split_string[0], float(split_string[1]), float(split_string[2].strip().replace('$', ''))])
+    return left
