47f3614124d5ab60c84f8a51b3d3b1a18c34160f
Edgar Guzman <eguzman@basecampcodingacademy.org>
2017-08-02 11:50:20 -0500

    I had to modify the funtion restock to make it work.

--- disk.py
+++ disk.py
@@ -8,30 +8,32 @@ def open_inventory():
         lines = file.readlines()
     for line in lines:
         split_string = line.strip().replace('$', '').split(', ')
-        inventory.append([split_string[0], float(split_string[1]), float(split_string[2]), float(split_string[3]), float(split_string[4])])
+        inventory.append([split_string[0], float(split_string[1]), float(split_string[2]), float(split_string[3]), float(split_string[4]), float(split_string[5])])
     return inventory
 
-def restock(inventory):
+def restock(gear_type, how_much):
     """ [] -> None
     creates new inventory and writes it to file 
     """
-    str_l = ['gear_type, amount, price, deposit, replacement']
+    str_l = ['gear_type, amount, price, deposit, replacement, gear number']
+    inventory = open_inventory()
     for item in inventory:
-        if int(item[1]) < 100:
-            (item[1]) = 100
-            item[1] = str(item[1])
-            item[2] = str(item[2])
-            item[3] = str(item[3])
-            item[4] = str(item[4])
+        if item[0] == gear_type:
+            (item[1]) = int(how_much) + int(item[1])
+        item[1] = str(item[1])
+        item[2] = str(item[2])
+        item[3] = str(item[3])
+        item[4] = str(item[4])
+        item[5] = str(item[5])
         str_l.append(', '.join(item))
-        message = '\n'.join(str_l)
-        return message  
+        message = '\n'.join(str_l)  
     with open('inventory.txt', 'w') as file:
         file.write(message)
 
 
+
 def takes_away(gear_type, amount):
-    str_l = ['gear_type, amount, price, deposit, replacement']
+    str_l = ['gear_type, amount, price, deposit, replacement, gear number']
     left = open_inventory()
     for item in left:
         if item[0] == gear_type:
@@ -44,12 +46,15 @@ def takes_away(gear_type, amount):
         item[2] = str(item[2])
         item[3] = str(item[3])
         item[4] = str(item[4])
+        item[5] = str(item[5])
         str_l.append(', '.join(item))
         message = '\n'.join(str_l)
 
     with open('inventory.txt', 'w') as file: 
         file.write(message)
     return True
+
+
 
 
 def keeps_history(gear_type, price):
@@ -66,6 +71,3 @@ def in_the_history():
         split_string = line.strip().split(', ')
         left.append([split_string[0], float(split_string[1]).strip().replace('$', '')])
     return left
-
-# def close_inventory(inventory):
-#     return None
