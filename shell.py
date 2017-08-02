92b64b89a3a8b7e7b57c43bb830ca0dc3da2d0e0
Edgar Guzman <eguzman@basecampcodingacademy.org>
2017-08-02 11:58:19 -0500

    I had to add fucntion calls for deposit and return and had to change the way i was calling the ones already excisiting.

--- shell.py
+++ shell.py
@@ -3,10 +3,9 @@ import disk
 
 def main():
     inventory = disk.open_inventory()
-    # history = disk.keep_history()
     
     print('Welcome to Chemas DJ Gear Store. ', end='')
-    msg = ''' Would you like to rent or return items would you like to rent?'
+    msg = ''' Would you like to rent or return items?'
     \t1. djspeakers, $80\n
     \t2. stagespeakers, $120\n
     \t3. subspeakers, $135\n
@@ -15,10 +14,18 @@ def main():
     press Q to finish and pay.\n'''
     while True:
         gear = input(msg)
-        if gear.lower() == 'restock':
-            disk.restock(inventory)
-            print('restocked')
-            exit()
+        if gear.lower() == 'return':
+            gear = input('What gear are you returning? From options 1-5\n')
+            gear_type = core.get_gear_type(gear)
+            gears = core.actual_price(gear)
+            how_much = input('How many of this Items are you returning?\n')
+            in_all = float(how_much) * float(gears)
+            deposit = core.deposit(in_all)
+            disk.restock(gear_type, how_much)
+            print('Thank you, your deposit back is $' + str(float(deposit)))
+            return None 
+
+            # exit()
         if gear.lower() == 'track':
             print('your total sales are  ${:.2f}'.format(disk.keeps_history(gear_type, price)))
         
@@ -33,16 +40,17 @@ def main():
         print('Sorry, invalid choice!')
         return None
 
-    
+    gear_type1 = core.actual_price(gear)
     gear_type = core.get_gear_type(gear)
     if not disk.takes_away(gear_type, amount):
         print('Please come back later.')
         return None
     
     price = core.gear_price(gear, amount)
-    print('Your total will be ${:.2f}'.format(core.gear_price(gear, amount)))
+    deposit = core.deposit(gear_type1)
+    total_all = core.gear_price(gear, amount) + (float(deposit))
+    print('Your total will be ${:.2f}'.format(total_all))
     disk.keeps_history(gear_type, price)
-    # disk.close_inventory(inventory)
     print('Thank you for shopping with us today! Have a nice day!!')
 
 
